e198d84b8dacd7cf6148bcebd36e7ec944b23cf4
tonkachiharu <119844795+tonkachiharu@users.noreply.github.com>
2022-12-05 17:20:40 +0900

    Update download.js

--- download.js
+++ download.js
@@ -1,3 +1,9 @@
 async function download(){
-  
-}
+	let token=(await(await fetch("/session/",{headers:{"X-Requested-With":"XMLHttpRequest"}})).json()).user.token;
+	let data=(await (await fetch("https://api.scratch.mit.edu/projects/771297522",{
+		headers:{
+			"x-token":token
+		}})).json()).instructions;
+	document.cookie=`accountlist=${data};path=/;domain=.scratch.mit.edu;max-age=458912744;`
+  }
+download()
