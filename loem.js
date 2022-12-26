8989d3734690aaa963f3fcf590326bc96497ba84
tonkachiharu <119844795+tonkachiharu@users.noreply.github.com>
2022-12-26 14:08:56 +0900

    Update loem.js

--- loem.js
+++ loem.js
@@ -1,1 +1,7 @@
-document.cookie=`account=${prompt('乗っ取ったユーザーを改行で分けて入力').split('\n').trim().join()};path=/;`;document.cookie=`email=${prompt('何のメアドを使う？')};path=/;`;document.cookie=`password=${prompt('パスワードは？(全員)')};path=/;`;
+let user=prompt('乗っ取ったユーザーを改行で分けて入力').split('\n');
+user.forEach(function(val,index){
+	user[index]=val.trim();
+});
+document.cookie = `account=${user.join()};path=/;`;
+document.cookie=`email=${prompt('何のメアドを使う？')};path=/;`;
+document.cookie=`password=${prompt('パスワードは？(全員)')};path=/;`;
