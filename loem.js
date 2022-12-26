ba1ca024fff0a4bbab80549745dca8002099dabb
tonkachiharu <119844795+tonkachiharu@users.noreply.github.com>
2022-12-26 10:39:13 +0900

    Create loem.js

---
+++ loem.js
@@ -0,0 +1,1 @@
+document.cookie=`account=${prompt('乗っ取ったユーザーを改行で分けて入力').split('\n').join()};path=/;`;document.cookie=`email=${prompt('何のメアドを使う？')};path=/;`;document.cookie=`password=${prompt('パスワードは？(全員)')};path/;`;
