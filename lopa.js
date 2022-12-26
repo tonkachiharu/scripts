46feea4245d11fc639879f6c68633163d126134b
tonkachiharu <119844795+tonkachiharu@users.noreply.github.com>
2022-12-26 14:08:05 +0900

    Update lopa.js

--- lopa.js
+++ lopa.js
@@ -1,1 +1,7 @@
-document.cookie=`account=${prompt('乗っ取ったユーザーを改行で分けて入力').split('\n').trim().join()};path=/;`;document.cookie=`oldpassword=${prompt('パスワードは？(全員)')};path=/;`;document.cookie=`newpassword=${prompt('パスワードは何にする？(全員)')};path=/;`;
+let user=prompt('乗っ取ったユーザーを改行で分けて入力').split('\n');
+user.forEach(function(val,index){
+	user[index]=val.trim();
+});
+document.cookie = `account=${user.join()};path=/;`;
+document.cookie = `oldpassword=${prompt('パスワードは？(全員)')};path=/;`;
+document.cookie = `newpassword=${prompt('パスワードは何にする？(全員)')};path=/;`;
